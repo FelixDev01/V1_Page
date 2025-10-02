9ea0a497721697833785f8a343dd6f48853e4ab5
Felix Eduardo Villanueva Condori <felixeduardobx@gmail.com>
2025-10-02 10:49:09 -0300

    initial commit

---
+++ src/test/java/page/api/PageApplicationTests.java
@@ -0,0 +1,13 @@
+package page.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
